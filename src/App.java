016bf4c6eaf4e3ace6c34c1f5fe19df79887f33e
matiasArr <matias.arriagada1801@alumnos.ubiobio.cl>
2021-11-20 13:29:33 -0300

    implementacion de factorial recursivo e iterativo

--- src/App.java
+++ src/App.java
@@ -19,15 +19,42 @@ public class App {
         double  n =  sc.nextDouble();
 
         resultado=coordinador(n);
+
         System.out.println("El resultado es: "+ resultado);
     }
     public static double coordinador(double num){
-        double n = num/2;
+
+        double res = iterativa((int)num);
+        double resu = recursiva((int )num);
+        return resu;
+
+        /*double n = num/2;
         double resu;
         proc hiloUno = new proc(n);
         hiloUno.start();
         resu = hiloUno.getNum();
         hiloUno.interrupt();
-        return resu;
+        return resu;*/
+    }
+    public static double iterativa(int nu){
+        double fa;
+        if(nu <= 2){
+            return nu;
+        }else{
+            fa = nu;
+            for (int i = nu; i >= 2; i--) {
+                fa = fa*(i-1);
+            }
+            return fa;
+        }
+    }
+    public static double recursiva(int num){
+        if(num == 0){
+            return 1;
+        }else{
+            System.out.println(num);
+            return num * recursiva(num-1);
+        }
+
     }
 }
