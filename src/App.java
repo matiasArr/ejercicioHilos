a8ff91afc71913a2eaf63e8a1595632148dcfa25
matiasArr <matias.arriagada1801@alumnos.ubiobio.cl>
2021-11-24 11:26:57 -0300

    resolviendo el metodo iterativo y recursivo

--- src/App.java
+++ src/App.java
@@ -1,60 +1,87 @@
 import java.util.Scanner;
 import javax.sound.sampled.SourceDataLine;
+import java.math.*;
 
 public class App {
+    /*
+      NOTAS PARA LA PROXIMA VUELTA
+
+     - HAY UN PROBLEMA CON EL METODO RECURSIVO(ALGO CON LA STACK) SOLUCION: -XSS
+     - EL METODO ITERATIVO LANZA UN ERROR CON EL TIPO LONG
+     - FALTA AGREGAR EL APARTADO DEL HILOS(SE CREAN DOS HILOS)
+     
+    */
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         double resultado;
-        /*proc hiloUno = new proc();
+
+        double start = System.currentTimeMillis();
+        long numero = 100L;
+        long res = iterativa(numero);
+        System.out.println(String.valueOf(res));  
+        double end = System.currentTimeMillis();
+
+        System.out.println("Y los milisegundos son: "+ (end-start));
+
+        /*
+        JUGANDO CON HILOS
+        proc hiloUno = new proc();
         procDos hiloDos = new procDos();
         hiloDos.start();
         //hiloUno.start();
-
         //hiloUno.interrupt();
         hiloDos.interrupt();
-        
-        */
-        
         System.out.println("dame un numero");
         double  n =  sc.nextDouble();
-
-        resultado=coordinador(n);
-
-        System.out.println("El resultado es: "+ resultado);
+        */
     }
-    public static double coordinador(double num){
+    public static long iterativa(long nu){
+        long res = 1L;
+        for (long i = 1L; i <= nu ; i++) {
+            res *= i;
+        }
+        return res;
+    }
+    //NO SE PUEDE JUGAR CON LOS BIG EN UN METODO RECURSIVO3
+    public static long recursiva(long num){
+        if (num == 1) {
+            return num;
+        } else {
+            return num * recursiva(num - 1);
+        }
+        // Creo un Big de uno para realizar las siguientes operaciones: 
+        //COMPARACION Y RESTA 
+       /* String uno = "1";
+        //BigInteger  bigUno = new BigInteger(uno);
+        if(num.equals(bigUno)){
+            return BigInteger.valueOf(1);
+        }else{
+            BigInteger resta = num.subtract(bigUno);
+            BigInteger multi = num.multiply(resta);
+            return multi;
+            //return num * recursiva(resta));//num -1
+        }*/
 
-        double res = iterativa((int)num);
-        double resu = recursiva((int )num);
-        return resu;
+    }
 
-        /*double n = num/2;
+    /*
+    
+    METODO QUE SE APLICARA PARA LOS HILOS
+
+    public static int factorial(int num){
+        
+        
+        //double res = iterativa((int)num);
+        
+
+        //double resu = recursiva((int )num);
+
+        ouble n = num/2;
         double resu;
         proc hiloUno = new proc(n);
         hiloUno.start();
         resu = hiloUno.getNum();
         hiloUno.interrupt();
-        return resu;*/
-    }
-    public static double iterativa(int nu){
-        double fa;
-        if(nu <= 2){
-            return nu;
-        }else{
-            fa = nu;
-            for (int i = nu; i >= 2; i--) {
-                fa = fa*(i-1);
-            }
-            return fa;
-        }
-    }
-    public static double recursiva(int num){
-        if(num == 0){
-            return 1;
-        }else{
-            System.out.println(num);
-            return num * recursiva(num-1);
-        }
-
-    }
+        return resu;
+    }*/
 }
